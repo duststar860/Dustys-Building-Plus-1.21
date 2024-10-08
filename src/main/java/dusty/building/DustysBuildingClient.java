867a3c031fae3c2488f9929ea2c9dfaeceb17709
DustyStarrz <duststar860@gmail.com>
2024-10-08 15:14:53 -0400

    Initial commit

---
+++ src/main/java/dusty/building/DustysBuildingClient.java
@@ -0,0 +1,10 @@
+package dusty.building;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DustysBuildingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
